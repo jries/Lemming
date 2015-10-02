8695cfa32c4ac427d36c8e001f6b7d4f7f992b58
chicoronny <Ronny.Sczech@biotec.tu-dresden.de>
2015-10-02 13:34:47 +0200

    Lemming is now a Fiji plug-in

---
+++ src/main/java/org/lemming/Lemming.java
@@ -0,0 +1,31 @@
+package org.lemming;
+
+import javax.swing.SwingUtilities;
+
+import org.lemming.gui.Controller;
+
+import ij.plugin.PlugIn;
+
+@SuppressWarnings("rawtypes")
+public class Lemming implements PlugIn {
+
+	@Override
+	public void run(String arg) {
+		Controller frame = new Controller();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Controller frame = new Controller();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
